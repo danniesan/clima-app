2fec4fd684d94d94f3ad3fc9a1fbd134e9c8abc5
danniesan <88257285+danniesan@users.noreply.github.com>
2024-07-02 23:31:44 -0400

    add search function using API

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "bfb46b3ac41f3bd0tof60adf87306491";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
-
-let apiKey = "bfb46b3ac41f3bd0tof60adf87306491";
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial";
+searchCity("Trenton");
